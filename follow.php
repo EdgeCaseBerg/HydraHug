0ee1f74d1de504f9c79bb712c8535f6b3e08ed72
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2014-11-12 17:26:41 -0500

    Fixed follow require

--- follow.php
+++ follow.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include dirname(__FILE__) . '/twitteroauth/twitteroauth/config.php';
+include dirname(__FILE__) . '/twitteroauth/config.php';
 include dirname(__FILE__) . '/twitteroauth/twitteroauth/twitteroauth.php';
 
 if(!isset($_GET['twitter_id']) || !is_numeric($_GET['twitter_id'])){

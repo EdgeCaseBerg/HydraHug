f5676c9b70f388a2854582beb37c6beca39755b1
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2014-11-13 16:30:37 -0500

    Fixing SERVER_NAME when ran from cron

--- twitteroauth/config.php
+++ twitteroauth/config.php
@@ -4,7 +4,9 @@ define ('CONSUMER_KEY', $conf->apiKey);
 define ('CONSUMER_SECRET', $conf->apiSecretKey);
 define ('OAUTH_TOKEN', $conf->accessToken);
 define ('OAUTH_TOKEN_SECRET', $conf->accessSecret);
-define ('OAUTH_CALLBACK', 'http://' . $_SERVER['SERVER_NAME'] . '/twitteroauth/callback.php' );
+if(isset($_SERVER['SERVER_NAME'])){
+	define ('OAUTH_CALLBACK', 'http://' . $_SERVER['SERVER_NAME'] . '/twitteroauth/callback.php' );
+}
 define ('DB_NAME', $conf->dbname);
 define ('DB_USER',$conf->dbuser);
 define ('DB_PASS',$conf->dbpass);

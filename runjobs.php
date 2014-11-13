937344c6e66321e2ca610dfb2bfd054836dc1d6d
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2014-11-13 16:55:18 -0500

    Newlines and query fix.

--- runjobs.php
+++ runjobs.php
@@ -9,21 +9,21 @@ mysql_select_db(DB_NAME, $dblink);
 /* Get unfinished jobs */
 /* Retrieve credentials for users */
 $jobSql = "SELECT jobs.id as jobid, last_id, owner_id as listOwnerToBeFollowedOwner,oauth_token,oauth_secret" 
-         ." FROM jobs join users ON users.twitter_id = jobs.follower_id WHERE status != FINISHED";
+         ." FROM jobs JOIN users ON users.twitter_id = jobs.follower_id WHERE status != 'FINISHED'";
 $jobRes = mysql_query($jobSql, $dblink);
 
 if($jobRes === FALSE){
-    echo 'jobRes query failed';
+    echo 'jobRes query failed' . '\n';
     exit;
 }
 
 /* Iterate through "to follows" from database */
-echo ("Beginning Job Processing " . mysql_num_rows($jobRes));
+echo ("Beginning Job Processing " . mysql_num_rows($jobRes)) . '\n';
 while (($jobInfo = mysql_fetch_object($jobRes)) != FALSE) {
     $jobid = $jobInfo->jobid;
     $jobStatus = "RUNNING";
     if ( mysql_query("UPDATE jobs SET status = \"$jobStatus\" WHERE id = $jobid", $dblink) === FALSE ){
-        echo 'Could not update job ' . $jobid . ' to running status';
+        echo 'Could not update job ' . $jobid . ' to running status' . '\n';
         continue;
     }
 
@@ -35,7 +35,7 @@ while (($jobInfo = mysql_fetch_object($jobRes)) != FALSE) {
     $followListSQL = "SELECT id,to_follow FROM lists WHERE owner_id = " . $jobInfo->listOwnerToBeFollowedOwner . " AND id >= $lastid ORDER BY id";
     $followRes = mysql_query($followListSQL, $dblink);
     if($followRes === FALSE){
-        echo 'Failed to select list owner';
+        echo 'Failed to select list owner' . '\n';
         continue;
     }
 
@@ -44,7 +44,7 @@ while (($jobInfo = mysql_fetch_object($jobRes)) != FALSE) {
 
         $twitter = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $jobInfo->oauth_token, $jobInfo->oauth_secret);
         if (!is_object($twitter)) {
-            echo('Error creating TwitterOAuth object');
+            echo('Error creating TwitterOAuth object') . '\n';
             exit (-1);
         }       
         $twitter->host = 'https://api.twitter.com/1.1/';
@@ -58,7 +58,7 @@ while (($jobInfo = mysql_fetch_object($jobRes)) != FALSE) {
         if (!is_object($followed) || isset($followed->errors)) {
             $ref = uniqid();
             mysql_query("UPDATE jobs SET status = \"ERROR\", message = \"Problem while processing, Ref: $ref\", last_id = {$followRow->id} WHERE id = $jobid", $dblink);
-            echo ($ref . ' ' . echo_r($followed));
+            echo ($ref . ' ' . echo_r($followed)) . '\n';
             goto next;
         } else {
             if($processed % 10){
@@ -70,6 +70,6 @@ while (($jobInfo = mysql_fetch_object($jobRes)) != FALSE) {
     mysql_query("UPDATE jobs SET status = \"FINISHED\", message = \"Done processing. Processed: $processed followers\", last_id = {$followRow->id} WHERE id = $jobid", $dblink);    
     next:
 }
-echo ("Done Running Jobs");
+echo ("Done Running Jobs") . '\n';
 mysql_close($dblink);
 ?>
